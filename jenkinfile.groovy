be94203f2d9ad97869c6d6d4430bbab8998fb216
seedoffd <seedoffd@gmail.com>
2019-05-04 15:11:49 -0500

    sudo added

--- jenkinfile.groovy
+++ jenkinfile.groovy
@@ -5,7 +5,7 @@ node{
     }
     stage("build application"){
         sh "scp -r * ec2-user@${ENV}:/tmp"
-        sh "ssh ec2-user@${ENV} pip install -r /tmp/requirements.txt"
+        sh "ssh ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
     }
     stage("App Run"){
         sh "ssh ec2-user@${ENV} python /tmp/app.py"
